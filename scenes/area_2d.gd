1190b4ed8d071bac9ae0b9f2bba6ac475a19df01
samuel <samuelamaralantunes@gmail.com>
2025-01-25 09:25:55 +0100

    aaaaaaaaaaaaaa

--- scenes/area_2d.gd
+++ scenes/area_2d.gd
@@ -1,16 +1,7 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+@export var boss: Node2D
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
-		$"../music1Boss".play()
+	print("a")
+	boss.process_mode = Node.PROCESS_MODE_INHERIT
