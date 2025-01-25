2cc8db25b25c86c203d02cf4b0dcb5bd2b144eae
jmentape <lohan.boeglin@etu.univ-nantes.fr>
2025-01-25 04:42:54 +0100

    reworking lvl1

--- scripts/tuba.gd
+++ scripts/tuba.gd
@@ -3,13 +3,39 @@ extends Character
 var State = CharacterStatus
 var FacingDirection
 var shock = preload("res://assets/fx/shockwave.tscn")
+var speed = 120
 @onready var SpriteSheet = $sprite
 
+var markers = []
+var current_marker_index = 0
 
+func _ready():
+	# Populate the markers array with marker positions
+	markers = [
+		$Marker3.global_position,
+		$Marker1.global_position,
+		$Marker2.global_position
+	]
+	# Optionally, move the character to the first marker initially
+	global_position = markers[0]
 
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		State  = CharacterStatus.ONGROUND
+	if current_marker_index < markers.size():
+		# Get the target position
+		var target = markers[current_marker_index]
+		# Calculate direction to target
+		var direction = (target - global_position).normalized()
+		# Move towards the target
+		if $shocktimer.is_stopped():
+			velocity = direction * speed
+			move_and_slide()
+
+		# Check if we reached the target
+		if global_position.distance_to(target) < 5:  # Tolerance for stopping
+			current_marker_index = (current_marker_index+1)%3
+		
 	
 		
 func _process(_delta) -> void:
