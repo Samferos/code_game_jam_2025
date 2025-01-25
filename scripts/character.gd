165a57480a0ca9f8a7ec905a83cd566f964c7904
samuel <samuelamaralantunes@gmail.com>
2025-01-25 02:24:16 +0100

    character: air control & wall jump balance

--- scripts/character.gd
+++ scripts/character.gd
@@ -6,23 +6,23 @@ class_name Character
 @export var SPEED = 5.0
 
 const RATIO = 1_000
-const RATIO_SQUARED = RATIO*RATIO
+const RATIO_SQUARED = RATIO * RATIO
 @export var AIR_CONTROL = 0.14
 @export var JUMP_FORCE = 29.5
 const AIR_DRAG_COEF = 0.42
 const GROUND_RES_COEF = 0.25
 @export var DASH_SPEED = 5
 
-var Velocity : Vector2
+var Velocity: Vector2
 var HoldingWall = false
 
-func take_damage(damage : float):
+func take_damage(damage: float):
 	CURRENT_HEALTH -= damage
 	
-func take_knockback(kb : Vector2):
+func take_knockback(kb: Vector2):
 	Velocity.x = kb.x
 
-func Move(direction:Vector2) -> void:
+func Move(direction: Vector2) -> void:
 	
 	if is_on_floor():
 		Velocity += direction * SPEED * RATIO
@@ -31,7 +31,7 @@ func Move(direction:Vector2) -> void:
 		HoldingWall = true
 	else:
 		var desired_air_control = direction * SPEED * RATIO * AIR_CONTROL
-		if Velocity.length() + desired_air_control.length() < 20.0 * RATIO:
+		if (Velocity + desired_air_control).length() < 15.0 * RATIO || Velocity.normalized().dot(desired_air_control.normalized()) < 0.1:
 			Velocity += desired_air_control
 
 
@@ -40,15 +40,16 @@ func Jump() -> void:
 		Velocity.y = -JUMP_FORCE * RATIO
 	elif is_on_wall_only():
 		Velocity = (get_wall_normal() + up_direction * 0.9).normalized() * JUMP_FORCE * RATIO
+		HoldingWall = false
 		
 		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		Velocity += get_gravity() 
-		var dragForce : Vector2
+		Velocity += get_gravity()
+		var dragForce: Vector2
 		if not HoldingWall:
-			var fluidRelativeVelocity : float = Velocity.length_squared() / RATIO_SQUARED
+			var fluidRelativeVelocity: float = Velocity.length_squared() / RATIO_SQUARED
 			dragForce = Velocity.normalized() * 0.5 * AIR_DRAG_COEF * fluidRelativeVelocity
 		else:
 			dragForce = Velocity * GROUND_RES_COEF * 2.0;
@@ -56,13 +57,13 @@ func _physics_process(delta: float) -> void:
 			
 		Velocity -= dragForce
 	else:
-		var resistance : Vector2 = Velocity * GROUND_RES_COEF
+		var resistance: Vector2 = Velocity * GROUND_RES_COEF
 		Velocity -= resistance
 	velocity = Velocity * delta
 	move_and_slide()
 
 	if is_on_ceiling():
-		Velocity.y=0
+		Velocity.y = 0
 		
 	if is_on_wall():
 		Velocity.x = 0
